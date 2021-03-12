3b0812913608fd487db61459feaef1c14367c561
daryl <tsdaryl@gmail.com>
2021-03-11 21:53:19 -0800

    add src/fakeAxios.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from './fakeAxios';
+// import axios from 'axios';
+
+axios.get('/api/users').then(data => console.log(data))
 
 function App() {
   return (
@@ -15,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!
         </a>
       </header>
     </div>
---
+++ src/fakeAxios.js
@@ -0,0 +1,45 @@
+// fake data
+const fakeData = {
+  '/api/users': {
+    id: 1,
+    first_name: 'Chuck',
+    last_name: 'Norris',
+    avatar: 'img link',
+    gender: 'Male',
+    // add more as needed
+  },
+  '/api/sessions': {
+    id: 1,
+    // add more as needed
+  }
+}
+
+// fake delayed promise
+const fakePromise = function(fakeData) {
+  const fakePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(fakeData);
+    }, 100);
+  });
+  return fakePromise;
+}
+
+// fake axios
+const axios = {
+  get: function(route) {
+    return fakePromise(fakeData[route]); 
+  }
+}
+
+
+// // usage
+
+// axios.get('/api/users')
+//   .then(data => {
+//     console.log(data);
+//   })
+
+
+export default axios;
+
+
