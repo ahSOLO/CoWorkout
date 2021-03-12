0491b2d52d71e2c55e9dbb0adbe5666cf2bd0a65
daryl <tsdaryl@gmail.com>
2021-03-12 10:11:44 -0800

    remove cookie functions temporarily

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import axios from './fakeAxios';
 // import axios from 'axios';
 import Cookies from 'universal-cookie';
 
-cookies.set('test', 'val');
 function App() {
   const checkLogin = function() {
     
