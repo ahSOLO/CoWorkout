8969fee0d459e82ca75b411d499f2afd006fe55c
Daryl <tsdaryl@gmail.com>
2021-03-12 22:24:44 -0800

    add basic exports to second-from-top level components

---
+++ src/components/Settings.js
@@ -0,0 +1,5 @@
+export default function Settings(props) {
+  return (
+    <h1>Settings</h1>
+  )
+}
---
+++ src/components/Sidebar.js
@@ -0,0 +1,5 @@
+export default function Sidebar(props) {
+  return (
+    <h1>Sidebar</h1>
+  )
+}
