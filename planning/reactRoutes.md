c6b253fd039f2bf67cddf8257fc187226e476c6b
Daryl <tsdaryl@gmail.com>
2021-03-12 20:32:18 -0800

    add MainRouter.js and planning/reactRoutes.md

---
+++ planning/reactRoutes.md
@@ -0,0 +1,16 @@
+# React App Routes
+
+* / - homepage (for users who are not registered)
+* /dashboard
+* /login
+* /logout
+* /register
+* /settings
+* /account
+* /leaderboard
+* /rewards
+* /friends
+* /profile
+* /profile/<user UUID>
+* /workout-call/
+
---
+++ src/components/MainRouter.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as router, Link, Route } from 'react-router-dom';
+
+const mainRouter = () => {
+  return (
+    <div>
+      <h2>Main Router</h2>
+
+      <Router>
+        <nav>
+        </nav>
+      </Router>
+    </div>
+  )
+
+}
+
+export default MainRouter;
