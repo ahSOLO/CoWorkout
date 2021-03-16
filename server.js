09a4d3e39194b5425ec8c2f6235a5cddd538749f
Ryan Kang <rkang626@gmail.com>
2021-03-16 20:31:10 +0000

    update backend server file

--- server.js
+++ server.js
@@ -2,16 +2,45 @@
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
-const express    = require("express");
-const app        = express();
+const PORT = 8081;
+const express = require("express");
+const app = express();
+const cors = require('cors')
+app.use(cors())
+
+// Twilio config
+const path = require('path');
+const AccessToken = require('twilio').jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+
+const MAX_ALLOWED_SESSION_DURATION = 14400;
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
+const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/token', (req, res) => {
+  const { identity, roomName } = req.query;
+  const token = new AccessToken(twilioAccountSid, twilioApiKeySID, twilioApiKeySecret, {
+    ttl: MAX_ALLOWED_SESSION_DURATION,
+  });
+  token.identity = identity;
+  const videoGrant = new VideoGrant({ room: roomName });
+  token.addGrant(videoGrant);
+  res.send(token.toJwt());
+  console.log(`issued token for ${identity} in room ${roomName}`);
+});
+
+app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
 
 // PG database client/connection setup
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const dbParams = require('./lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
+// Mount resource routes
 const users = require("./routes/users");
 const sessions = require("./routes/sessions");
 const workout_interests = require("./routes/workout_interests");
@@ -21,7 +50,6 @@ const user_workout_goals = require("./routes/user_workout_goals");
 const session_users = require("./routes/session_users");
 const ratings = require("./routes/ratings");
 
-// Mount all resource routes
 app.use("/api/users", users(db));
 app.use("/api/sessions", sessions(db));
 app.use("/api/workout_interests", workout_interests(db));
@@ -30,6 +58,7 @@ app.use("/api/user_workout_interests", user_workout_interests(db));
 app.use("/api/user_workout_goals", user_workout_goals(db));
 app.use("/api/session_users", session_users(db));
 app.use("/api/ratings", ratings(db));
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
