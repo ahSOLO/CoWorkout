9b92262550e24ee7329776e9ff5119d64ed1a0b4
ahSOLO <junmenghan@gmail.com>
2021-03-16 12:27:03 -0700

    add theme and custom colors / fonts

--- src/App.js
+++ src/App.js
@@ -4,9 +4,46 @@ import './App.scss';
 import MainRouter from './components/MainRouter';
 import './basic.scss';
 
+// mat-ui theme
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core'
+
 // How to import a mat-ui component + Icon
 import Button from '@material-ui/core/Button';
 import FaceIcon from '@material-ui/icons/Face';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#FBB03B'
+    },
+    secondary: {
+      main: '#23262A'
+    }
+  },
+  typography: {
+    fontFamily: "'Montserrat', 'sans-serif'",
+    fontWeightBold: 600,
+    h1: {
+      fontFamily: "'Raleway', sans-serif"
+    },
+    h2: {
+      fontFamily: "'Raleway', sans-serif"
+    },
+    h3: {
+      fontFamily: "'Raleway', sans-serif"
+    },
+    h4: {
+      fontFamily: "'Raleway', sans-serif"
+    },
+    h5: {
+      fontFamily: "'Raleway', sans-serif"
+    },
+    h6: {
+      fontFamily: "'Raleway', sans-serif"
+    }
+  }
+})
 
 function App() {
   console.log('Rerendered');
@@ -19,13 +56,15 @@ function App() {
   // }();
 
   return (
-    <div>
-      <nav>CoWorkout</nav>
-      <MainRouter />
-      <br/> <br/>
-      {/* Example of mat-UI button + how to use an Icon */}
-      <Button color='primary' variant='contained' size='large' startIcon={<FaceIcon/>}>Example mat-UI Button</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <nav>CoWorkout</nav>
+        <MainRouter />
+        <br/> <br/>
+        {/* Example of mat-UI button + how to use an Icon */}
+        <Button color='primary' variant='contained' size='large' startIcon={<FaceIcon/>}>Example mat-UI Button</Button>
+      </div>
+    </ThemeProvider>
   );
 }
 
