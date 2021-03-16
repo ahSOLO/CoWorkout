68c18d64ff0a67c67492b565beafaffdd16b3b2c
ahSOLO <junmenghan@gmail.com>
2021-03-15 18:21:19 -0700

    WIP calendar implementation

---
+++ src/components/Dashboard/Calendar/Day/Slot/Index.js
@@ -0,0 +1,81 @@
+import "./styles.scss"
+
+export default function Slot(props) {
+  return (
+    <>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    <div className="slot">
+      Test
+    </div>
+    </>
+  )
+}
+
