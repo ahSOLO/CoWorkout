cd51d992bd40f6b7237d65941d922caeee3a6d94
Daryl <tsdaryl@gmail.com>
2021-03-15 17:10:41 -0700

    update fakeAxios and Day/index.js to use fake api call for sessions

--- src/fakeAxios.js
+++ src/fakeAxios.js
@@ -9,16 +9,40 @@ const fakeData = {
       gender: 'Male',
       // add more as needed
     },
-    '/api/sessions': {
-      id: 1,
-      // add more as needed
-    }
+    '/api/sessions': [
+      // FILTER RESULTS ON API SERVER SIDE
+      {
+        scheduled_at: '2021-03-15 12:00:00',
+        state: 'pending',
+        scheduled_duration: 30,
+        owner_id: 1,
+        owner_first_name: 'Chuck',
+        owner_avatar: 'image'
+      },
+      {
+        scheduled_at: '2021-03-15 12:00:00',
+        state: 'pending',
+        scheduled_duration: 30,
+        owner_id: 1,
+        owner_first_name: 'Chuck',
+        owner_avatar: 'image'
+      },
+      {
+        scheduled_at: '2021-03-15 12:00:00',
+        state: 'pending',
+        scheduled_duration: 30,
+        owner_id: 1,
+        owner_first_name: 'Chuck',
+        owner_avatar: 'image'
+      },
+    ]
   },
 
   'POST': {
     '/api/': {
       test: true
-    }
+    },
+    
   },
 
   'DELETE': {
@@ -40,13 +64,13 @@ const fakePromise = function(fakeData) {
 
 // fake axios
 const axios = {
-  get: function(route) {
+  get: function(route, params) {
     return fakePromise(fakeData['GET'][route]); 
   },
-  post: function(route) {
+  post: function(route, params) {
     return fakePromise(fakeData['POST'][route]); 
   },
-  delete: function(route) {
+  delete: function(route, params) {
     return fakePromise(fakeData['DELETE'][route]); 
   }
 }
