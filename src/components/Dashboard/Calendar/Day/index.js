2375b785165ceb5e5f3ac1736a5d11d66bac7bc3
ahSOLO <junmenghan@gmail.com>
2021-03-15 19:21:08 -0700

    replace divs with semantic variants, generate slots using .map

--- src/components/Dashboard/Calendar/Day/index.js
+++ src/components/Dashboard/Calendar/Day/index.js
@@ -4,9 +4,16 @@ import { Typography } from "@material-ui/core";
 import "./styles.scss";
 
 export default function Day(props) {
+  // 42 15-minute intervals in a day
+  const intervals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
+  const slots = intervals.map( interval => {
+    return (
+      <Slot content={interval} />
+    )
+  })
   return (
     <div className="container__slots">
-      <Slot/>
+      {slots}
     </div>
   )
 }
