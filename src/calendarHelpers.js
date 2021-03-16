955c281af45828b530bf7509a8816fc970255d50
Daryl <tsdaryl@gmail.com>
2021-03-16 00:29:12 -0700

    add calendarHelpers.js in /src folder

---
+++ src/calendarHelpers.js
@@ -0,0 +1,48 @@
+const extractTimeString = function(timestamp) {
+  
+};
+
+const changeToUserTZ = function(timestamp) {
+
+};
+
+const generateTimeString = function(hour, minute) {
+  let hourString = hour.toString();
+  let minuteString = minute.toString();
+  if (hourString.length !== 2) {
+    hourString = "0" + hourString;
+  }
+  if (minuteString.length !== 2) {
+    minuteString = "0" + minuteString;
+  }
+  return hourString + ':' + minuteString;
+}
+
+const autoGenerateEmptyAppointments = function() {
+  let emptyAppointments = {};
+  const weekDays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+  for (const day of weekDays) {
+    emptyAppointments[day] = {};
+    let hour = 0;
+    let minute = 0;
+    
+    while (hour < 24) {
+      let timeString = generateTimeString(hour, minute);
+      emptyAppointments[day][timeString] = { 'status': 'empty' };
+      minute += 15;
+      if (minute === 60) {
+        hour += 1;
+        minute = 0;
+      }
+    };
+  }
+  return emptyAppointments;
+}
+
+const allAppointments = autoGenerateEmptyAppointments();
+
+module.exports = {
+  allAppointments,
+  extractTimeString,
+  changeToUserTZ
+}
