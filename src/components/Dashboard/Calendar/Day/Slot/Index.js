9186b359c7e02dfba729592fd452eee58ec56b0f
ahSOLO <junmenghan@gmail.com>
2021-03-16 13:49:00 -0700

    add new slot components

--- src/components/Dashboard/Calendar/Day/Slot/Index.js
+++ src/components/Dashboard/Calendar/Day/Slot/Index.js
@@ -1,7 +1,18 @@
-import { Typography } from "@material-ui/core"
-import "./styles.scss"
+import { useState } from "react";
+
+import { Typography } from "@material-ui/core";
+import "./styles.scss";
+
+const EMPTY = "EMPTY";
+const BOOKED = "BOOKED";
+const MATCHING = "MATCHING";
+const MATCHED = "MATCHED";
+const LOADING = "LOADING";
+const ERROR = "ERROR";
 
 export default function Slot(props) {
+  const [mode, setMode] = useState(EMPTY);
+
   return (
     <div className="slot">
       <Typography>
