f0f3523647e5dca760f386d583d80cdd914a50c4
ahSOLO <junmenghan@gmail.com>
2021-03-16 19:22:21 -0700

    allow users to view profiles of their matches

--- src/components/Dashboard/Calendar/Day/Slot/Matched.js
+++ src/components/Dashboard/Calendar/Day/Slot/Matched.js
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import { Box, Avatar, Typography } from "@material-ui/core";
 import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
+import { hoverHandler } from "../../../../../helpers/utility";
 
 export default function Matched(props){
+  const [leftHover, setLeftHover] = useState(false);
+
   return(
     <Box display="flex" justifyContent="space-between" width="100%" alignItems="center">
       <div className="slot__booked">
         <div className="slot__avatar">
-          <Avatar src="https://i.pravatar.cc/300"/>
+          <Avatar 
+            src="https://i.pravatar.cc/300"
+            className="clickable"
+            onMouseEnter = {() => hoverHandler(setLeftHover, true)}
+            onMouseLeave = {() => hoverHandler(setLeftHover, false)}
+          />
         </div>
         <div className="slot__name">
           <Typography variant="body2">
-            Match Found!
+            {leftHover ? "View Profile?" : "Match Found!"}
           </Typography>
         </div>
       </div>
