6fe988012b13284eb27fbb125b1c664436892137
Daryl <tsdaryl@gmail.com>
2021-03-17 20:52:52 -0700

    implement range selection in backend route /api/sessions

--- routes/sessions.js
+++ routes/sessions.js
@@ -3,7 +3,29 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM sessions;`)
+    console.log('sessions');
+
+    let start_time;
+    let end_time;
+    if (!req.query.start_time || !req.query.end_time) {
+      start_time = new Date();
+      days_before_sat = 5 - start_time.getDay();
+      end_time = new Date();
+      end_time.setTime(end_time.getTime() + days_before_sat * 24 * 60 * 60 * 1000);
+    } else {
+      start_time = req.query.start_time;
+      end_time = req.query.end_time;
+    }
+    
+    // console.log(start_time, end_time);
+
+    const query = `SELECT * FROM sessions
+    WHERE sessions.scheduled_at >= '${start_time}'
+    AND sessions.scheduled_at <= '${end_time}';`;
+
+    console.log(query);
+
+    db.query(query)
       .then(data => {
         const sessions = data.rows;
         res.json({ sessions });
