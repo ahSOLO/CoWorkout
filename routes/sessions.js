bd335dfd0e1d98e204d39a81da63ea3c3db8c4e6
Ryan Kang <rkang626@gmail.com>
2021-03-18 20:16:09 +0000

    update api/sessions GET route

--- routes/sessions.js
+++ routes/sessions.js
@@ -3,8 +3,12 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+
     console.log('sessions');
 
+    const user_id = req.query.user_id;
+    const session_type = req.query.session_type;
+    const filter = req.query.filter; 
     let start_date;
     let end_date;
     if (!req.query.start_date || !req.query.end_date) {
@@ -16,18 +20,50 @@ module.exports = (db) => {
       start_date = new Date(req.query.start_date);
       end_date = new Date(req.query.end_date);
     }
-    
-    // console.log(start_date, end_date);
 
-    const query = `
-    SELECT * 
-    FROM sessions
-    WHERE sessions.scheduled_at >= '${start_date.toISOString()}'
-    AND sessions.scheduled_at <= '${end_date.toISOString()}';`;
+    let query_string;
 
-    console.log(query);
+    if (filter === 'calendar') {
+      if (session_type === 'transient') {
+        query_string = `
+        SELECT sessions.id AS session_id
+             , ARRAY_AGG(session_users.user_id ORDER BY session_users.user_id) as session_users
+             , sessions.scheduled_at as start_time
+             , workout_types.type as workout_type
+          FROM sessions 
+          JOIN session_users
+               ON sessions.id = session_users.session_id
+          LEFT JOIN workout_types
+               ON sessions.workout_type_id = workout_types.id
+         WHERE sessions.state = 'pending'
+           AND sessions.scheduled_at >= '${start_date.toISOString()}'
+           AND sessions.scheduled_at <= '${end_date.toISOString()}'
+           AND sessions.owner_id != ${user_id}
+         GROUP BY 1, 3, 4
+        HAVING COUNT(DISTINCT session_users.user_id) = 1;
+        `
+      } else {
+        query_string = `
+        SELECT sessions.id AS session_id
+             , ARRAY_AGG(session_users.user_id ORDER BY session_users.user_id) as session_users
+             , sessions.scheduled_at as start_time
+             , workout_types.type as workout_type
+         FROM sessions
+         JOIN (SELECT session_id FROM session_users WHERE user_id = ${user_id}) us
+              ON sessions.id = us.session_id
+         JOIN session_users
+              ON sessions.id = session_users.session_id
+         LEFT JOIN workout_types
+              ON sessions.workout_type_id = workout_types.id
+        WHERE sessions.state = 'pending'
+          AND sessions.scheduled_at >= '${start_date.toISOString()}'
+          AND sessions.scheduled_at <= '${end_date.toISOString()}'
+        GROUP BY 1, 3, 4
+        `
+      }
+    }
 
-    db.query(query)
+    db.query(query_string)
       .then(data => {
         const sessions = data.rows;
         res.json({ sessions });
