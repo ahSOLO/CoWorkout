c73c8d2ec4a41570b2202be24a82d2685dd71f18
ahSOLO <junmenghan@gmail.com>
2021-03-19 12:58:08 -0700

    change useApplicationData to use server data

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import axios from 'fakeAxios';
-// import axios from 'axios';
+// import axios from 'fakeAxios';
+import axios from 'axios';
 import { allSlots, rebuildAppointmentObjs, formatTimeStamp } from "helpers/calendarHelpers";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export default function useApplicationData() {
 
@@ -20,24 +22,29 @@ export default function useApplicationData() {
     const end_date = new Date(start_date.getTime() + start_date_days_from_sunday * 24 * 60 * 60 * 1000);
 
     console.log('START:', start_date, '\n', 'END:', end_date);
-    const baseURL = "";
     Promise.all([
-      axios.get(baseURL + '/api/sessions', {
+      axios.get(BASE_URL + '/api/sessions', {
         params: {
+          user_id: 1,
+          filter: "transient",
           start_date,
-          end_date
+          end_date,
         }
       }),
-      axios.get(baseURL + '/api/sessions', {
+      axios.get(BASE_URL + '/api/sessions', {
         params: {
-          current_user: []
+          user_id: 1,
+          filter: "persistent",
+          start_date,
+          end_date,
         }
       })
     ])
     .then((all) => {
       const [ allSessionsQuery, persistentSessionsQuery ] = all;
-      let retrievedAppointments = allSessionsQuery.data.rows;
-      let persistentAppointments = persistentSessionsQuery.data.rows;
+      console.log(allSessionsQuery);
+      let retrievedAppointments = allSessionsQuery.data.sessions;
+      let persistentAppointments = persistentSessionsQuery.data.sessions;
       // console.log(retrievedAppointments);
       setAppointments(prev => retrievedAppointments);
       setSlots(rebuildAppointmentObjs(slots, persistentAppointments, retrievedAppointments, 'America/Vancouver')); // !! modify to use user's timezone dynamically
