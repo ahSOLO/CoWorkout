58ecd8aa8557f4ef3653fc7e03fcf50a01c38e41
ahSOLO <junmenghan@gmail.com>
2021-03-19 16:51:37 -0700

    prevent users from booking on previous days

--- src/components/Dialogs/BookDialog.js
+++ src/components/Dialogs/BookDialog.js
@@ -16,12 +16,14 @@ export default function BookDialog(props) {
   const [activity, setActivity] = useState("0");
   const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
   const [time, setTime] = useState(moment().add(1, 'hour').startOf('hour'));
+  const [yesterday, setYesterday] = useState(moment().subtract(1,'days'));
 
   useEffect(() => {
     if (props.data){
       setActivity(props.data.activity_type || 0);
       setDate(moment(props.date).format("YYYY-MM-DD"));
       setTime(time.set({ "hour": props.data.hour, "minute": props.data.minute }))
+      setYesterday(moment().subtract(1,'days'));
     }
   }, [props.data, props.date])
 
@@ -85,6 +87,8 @@ export default function BookDialog(props) {
                 margin="normal"
                 id="date-picker-inline"
                 label="Please select a date"
+                minDate={yesterday.format()}
+                minDateMessage="Please select a time in the future"
                 value={date}
                 inputValue={date}
                 onChange={(d) => setDate(moment(d).format("YYYY-MM-DD"))}
