48db9d06795d3297eff733f97c69d2cdb039db8c
daryl <tsdaryl@gmail.com>
2021-03-22 13:43:17 -0700

    initial vc integration

---
+++ src/components/WorkoutCall/Session.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import AV from './AV';
+
+export default function Session(props) {
+
+  const { roomName, room, endSession } = props;
+  const [participants, setParticipants] = useState([]);
+
+  useEffect(() => {
+    const participantConnected = (participant) => {
+      setParticipants((prevParticipants) => [...prevParticipants, participant]);
+    };
+
+    const participantDisconnected = (participant) => {
+      setParticipants((prevParticipants) =>
+        prevParticipants.filter((p) => p !== participant)
+      );
+    };
+
+    room.on("participantConnected", participantConnected);
+    room.on("participantDisconnected", participantDisconnected);
+    room.participants.forEach(participantConnected);
+    return () => {
+      room.off("participantConnected", participantConnected);
+      room.off("participantDisconnected", participantDisconnected);
+    };
+  }, [room]);
+
+  const remoteParticipants = participants.map((participant) => (
+    <AV key={participant.sid} participant={participant} />
+  ));
+
+  return (
+    <div className="room">
+      <h2>Room: {roomName}</h2>
+      <button onClick={endSession}>Leave Session</button>
+      <div className="local-participant">
+        {room ? (
+          <AV
+            key={room.localParticipant.sid}
+            participant={room.localParticipant}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+
+      <h3>Remote Participants</h3>
+      <div className="remote-participants">{remoteParticipants}</div>
+    </div>
+  );
+};
--- src/components/WorkoutCall/index.js
+++ src/components/WorkoutCall/index.js
@@ -1,5 +1,98 @@
+import { useState, useCallback, useEffect } from 'react';
+import Session from './Session';
+import Video from 'twilio-video';
+import Preview from './Preview';
+
+// generate token here, no need for external route
+const AccessToken = require('twilio').jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+
 export default function WorkoutCall(props) {
-  return (
-    <h1>WorkoutCall</h1>
-  )
-}
+  const REACT_APP_TWILIO_ACCOUNT_SID = process.env.REACT_APP_TWILIO_ACCOUNT_SID;
+  const REACT_APP_TWILIO_API_KEY_SID = process.env.REACT_APP_TWILIO_API_KEY;
+  const REACT_APP_TWILIO_API_KEY_SECRET = process.env.REACT_APP_TWILIO_API_SECRET;
+
+  const [username, setUsername] = useState("testUser");
+  const [roomName, setRoomName] = useState("testRoom");
+  const [room, setRoom] = useState(null);
+  const [connecting, setConnecting] = useState(false);
+
+  const connectToRoom = useCallback(
+    async (event) => {
+      event.preventDefault();
+      setConnecting(true);
+      const JWT = createJwt(username, roomName);
+      Video.connect(JWT, {
+        name: roomName,
+      })
+        .then((room) => {
+          setConnecting(false);
+          setRoom(room);
+        })
+        .catch((err) => {
+          console.error(err);
+          setConnecting(false);
+        });
+    },
+    []
+  );
+
+  const endSession = useCallback(() => {
+    setRoom((prevRoom) => {
+      if (prevRoom) {
+        prevRoom.localParticipant.tracks.forEach((trackPub) => {
+          trackPub.track.stop();
+        });
+        prevRoom.disconnect();
+      }
+      return null;
+    });
+  }, []);
+
+  // create access token for user
+  const createJwt = function(identity = 'testuser', roomName = 'testroom') {
+    const token = new AccessToken(REACT_APP_TWILIO_ACCOUNT_SID, REACT_APP_TWILIO_API_KEY_SID, REACT_APP_TWILIO_API_KEY_SECRET, { ttl: 14400 });
+    token.identity = identity;
+    const videoGrant = new VideoGrant({ room: roomName });
+    token.addGrant(videoGrant);
+    console.log(`Generated token for ${identity} to join ${roomName}`);
+    return token.toJwt();
+  };
+
+  useEffect(() => {
+    if (room) {
+      const tidyUp = (event) => {
+        if (event.persisted) {
+          return;
+        }
+        if (room) {
+          endSession();
+        }
+      };
+      window.addEventListener("pagehide", tidyUp);
+      window.addEventListener("beforeunload", tidyUp);
+      return () => {
+        window.removeEventListener("pagehide", tidyUp);
+        window.removeEventListener("beforeunload", tidyUp);
+      };
+    }
+  }, [room, endSession]);
+
+  let render;
+  if (room) {
+    render = (
+      <Session roomName={roomName} room={room} endSession={endSession} />
+    );
+  } else {
+    const fakeLocalParticipant = {
+      'identity': 'Video Preview'
+    }
+    render = (
+      <>
+        <Preview participant={fakeLocalParticipant}/>
+        <button onClick={(event) => {connectToRoom(event)}}>I'm Ready!</button>
+      </>
+    );
+  }
+  return render;
+};
