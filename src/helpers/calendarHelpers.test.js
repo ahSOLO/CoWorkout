eebf5ba088b11de418b83e41a28f60adeaec097b
Daryl <tsdaryl@gmail.com>
2021-03-24 22:58:43 -0700

    server-ready

--- src/helpers/calendarHelpers.test.js
+++ src/helpers/calendarHelpers.test.js
@@ -1,63 +1,6 @@
 const { extractTimeString, extractDayOfWeek, changeToUserTZ, getWeekDates, rebuildAppointmentObjs, allSlots } = require('./calendarHelpers');
 
-const fakeSessions = [
-  // 3 appointments on the same timeslot
-  {
-    id: 1,
-    owner_first_name: 'Chuck',
-    owner_profile_image_url: 'avatar',
-    owner_ref_id: 'd93ghjfek',
-    start_time: '2021-03-15T16:00:00.000Z',
-    activity_type: 'napping'
-  },
-  {
-    id: 2,
-    owner_first_name: 'Rick',
-    owner_profile_image_url: 'avatar',
-    owner_ref_id: 'dsa98f89b',
-    start_time: '2021-03-15T16:00:00.000Z',
-    activity_type: 'lounging'
-  },
-  {
-    id: 3,
-    owner_first_name: 'Morty',
-    owner_profile_image_url: 'avatar',
-    owner_ref_id: 'fd938hgds',
-    start_time: '2021-03-15T16:00:00.000Z',
-    activity_type: 'sleeping'
-  },
-  // 1 appointment with no dups
-  {
-    id: 4,
-    owner_first_name: 'Coolguy',
-    owner_profile_image_url: 'avatar',
-    owner_ref_id: 'fdsegfgr0a',
-    start_time: '2021-03-15T16:30:00.000Z',
-    activity_type: 'sleeping'
-  },
-  // 2 more dup appointments
-  {
-    id: 5,
-    owner_first_name: 'Dup1',
-    owner_profile_image_url: 'avatar',
-    owner_ref_id: 'hjy5eje5',
-    start_time: '2021-03-15T17:30:00.000Z',
-    activity_type: 'sleeping'
-  },
-  {
-    id: 6,
-    owner_first_name: 'Dup2',
-    owner_profile_image_url: 'avatar',
-    owner_ref_id: 'gvrf43hj6',
-    start_time: '2021-03-15T17:30:00.000Z',
-    activity_type: 'sleeping'
-  },
-];
-
-const allAppointments = rebuildAppointmentObjs(allSlots, fakeSessions, 'Asia/Singapore');
-
-// console.log(allAppointments['WED'])
-
+console.log(allSlots);
 
 
 
